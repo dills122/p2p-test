ee2acc241b3ee9914ab6548e6f57194d75b6333a
Dylan Steele <dylansteele57@gmail.com>
2021-12-23 22:48:42 -0500

    added interactive shell to start cmd

--- cmd/node/start.go
+++ cmd/node/start.go
@@ -5,8 +5,12 @@ Copyright © 2021 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
 
+	commCmd "github.com/dills122/p2p-test/cmd/p2pc"
 	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 )
@@ -17,8 +21,29 @@ var startCmd = &cobra.Command{
 	Short: "start node with interactive shell",
 	Long:  ``,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("start called")
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			fmt.Print("$ ")
+			cmdString, err := reader.ReadString('\n')
+			if err != nil {
+				fmt.Fprintln(os.Stderr, err)
+			}
+			runCommand(cmdString)
+		}
 	},
+}
+
+func runCommand(commandStr string) {
+	commandStr = strings.TrimSuffix(commandStr, "\n")
+	arrCommandStr := strings.Fields(commandStr)
+	switch arrCommandStr[0] {
+	case "exit":
+		os.Exit(0)
+	case "start":
+		commCmd.Execute()
+	default:
+		fmt.Println("Unknown command")
+	}
 }
 
 func init() {
